ebdcd788663e2c1d65c2a11dae38b76de5251b87
Aness Chelfat <106586310+0xpix@users.noreply.github.com>
2026-09-10 17:34:50 +0200

    Fix pointer event compile import

--- app/src/main/java/com/pix/folio/ui/V08DashboardComponents.kt
+++ app/src/main/java/com/pix/folio/ui/V08DashboardComponents.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.input.pointer.awaitPointerEvent
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.font.FontWeight
